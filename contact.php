79afe63107a21611222f52b34ddc200bb89d0317
idbaali <ikebaalidavid@live.de>
2023-02-06 11:52:50 +0100

    phpOpenclass

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Formulaire de Contact</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+    <?php include_once('header.php'); ?>
+        <h1>Contactez nous</h1>
+        <form action="submit_contact.php" method="GET">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email">
+    </div>
+    <div>
+        <label for="message">Votre message</label>
+        <textarea placeholder="Exprimez vous" name="message"></textarea>
+    </div>
+    <button type="submit">Envoyer</button>
+</form>
+        <br />
+    </div>
+    
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- <footer id="pied_de_page">
+        <p>Copyright moi, tous droits réservés</p>
+</footer>
+     -->
+
+
+
+     <!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+  </div>
+</footer>
